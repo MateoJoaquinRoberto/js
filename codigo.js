696c720883f2971cad1cf0031ecfa3717b4776a7
Mateo <mateojoaquinroberto@hotmail.com>
2022-08-07 20:38:43 -0300

    Entega1

--- codigo.js
+++ codigo.js
@@ -1,34 +1,154 @@
-const listadoDeJuegos=["","Red dead redemption 2 " ,
-" The last Of Us ", 
-" Crushaders Kings III"]
+//Objetos
+const Juegos=[
+  {
+    nombre:" Red dead redemption 2 ",
+    tipo:"AAA",
+    modo:"Singleplayer y Multiplayer",
+    desarrollador:"Rockstar",
+    precio:2700
+  },
+  
+  {
+    nombre:" The last Of Us ",
+    tipo:"Aventura",
+    modo:"Singleplayer",
+    desarrollador:"Nautydog",
+    precio:1500
+},
 
+{
+  nombre:" Crushaders Kings III ",
+  tipo:"Estrategia",
+  modo:"Singleplayer y Multiplayer",
+  desarrollador:"Paradox",
+  precio:3800
+},
 
-let pasadoAString=listadoDeJuegos.join(" \n ");
-alert("Los juegos en stock son: "+pasadoAString)
+{
+  nombre:" Sims 4",
+  tipo:"Simulador",
+  modo:"Singleplayer",
+  desarrollador:"Ea",                                        
+  precio:2000
+},
 
-function Juegos(nombre, tipo, modo, desarrollador, precio){
-  this.nombre = nombre;
-  this.tipo = tipo;
-  this.modo = modo;
-  this.desarrollador = desarrollador;
-  this.precio = precio;
-  this.info = function(){ alert(this.nombre +  this.tipo +  this.modo +  this.desarrollador + this.precio )};
-  this.Info2 = function(){alert("El precio final con Iva sera de "+precio*1.75)}
+{
+  nombre:" Gta V ",
+  tipo:"Aventura",
+  modo:"Singleplayer y Multiplayer",
+  desarrollador:"Rockstar",
+  precio:3000
+},                                                      
+
+{
+  nombre:" NBA 2k23 ",
+  tipo:"Deportes",
+  modo:"Singleplayer y Multiplayer",
+  desarrollador:"2k",
+  precio:4000
+},
+
+{
+  nombre:" Hunt ",
+  tipo:"Shotter",
+  modo:"Singleplayer y Multiplayer",
+  desarrollador:"Davon",
+  precio:1000
+},
+
+{
+  nombre:" Stray ",
+  tipo:"Puzzles",
+  modo:"Singleplayer",
+  desarrollador:"Momon",
+  precio:500
+},
+
+];
+
+//agregar al carrito
+
+let compras=[]
+
+let VideosJuegos = prompt("Queres ver el catalogo de juegos?(Si o No) ")
+
+while(VideosJuegos!="Si" && VideosJuegos!="No"){
+  alert("Ingresa Si o No")
+  VideosJuegos = prompt("Queres ver el catalogo de juegos?(Si o No) ")
 }
 
-const juego1 = new Juegos ("Red Dead Redemption 2 ; ","AAA ; ","Singleplayer y Multiplayer ; ","Rockstar ; ",2700)
-const juego2 = new Juegos ("The last Of Us ; ","Aventura ; ","Singleplayer ; ","Nautydog ; ",1500)
-const juego3 = new Juegos ("Crushaders Kings III ; ","Estrategia ; ","Singleplayer y Multiplayer ; ","Paradox ; ",2000)
+if(VideosJuegos=="Si"){
+  alert("Lista de Videojuegos disponibles a continuacion")
+  let TodosLosJuegos = Juegos.map((Juego)=>Juego.nombre + " " + "$" +Juego.precio  );
+alert(TodosLosJuegos.join("-"))
+} else if (VideosJuegos =="No"){ 
+alert("Gracias por visitarnos")
+}
 
-juego1.info();
-juego1.Info2();
-juego2.info();
-juego2.Info2();
-juego3.info();
-juego3.Info2();
+while(VideosJuegos!= "No"){
+  let juego = prompt("Agrega un Videojuego de la lista que quieras")
+  let precio = 0
+
+  if(juego == "Red dead redemption 2" || juego == "The last Of Us" || juego == "Crushaders Kings III"  || 
+  juego == "Sims 4" || juego == "Gta V" || juego == "NBA 2k23" || juego == "Hunt" || juego == "Stray") {  
+    switch(juego) { 
+    case "Red dead redemption 2":
+      precio = 2700
+      break;
+    case "The last Of Us":
+      precio = 1500;
+      break;
+    case "Crushaders Kings III":
+      precio = 3000;
+      break;
+    case "Sims 4":
+      precio = 2000;
+      break;
+    case "Gta V":
+      precio = 3000;
+      break;
+    case "NBA 2k23":
+      precio = 4000;
+      break;
+    case "Hunt":
+      precio = 1000;
+      break;
+    case "Stray":
+      precio = 500;
+      break;    
+    default:
+      break;          
+      
+    } 
+  compras.push({juego, precio}); 
+  console.log(compras)
+  } else{
+    alert("Juego no disponible")
+  }
+  VideosJuegos = prompt("Queres agregar otro juego al carrito?(Si o No)")
+
+  while(VideosJuegos == "No"){
+    alert("Gracias por tu compra,tu juego esta en tu carrito")
+    compras.forEach((CompraTerminada)=>{
+      console.log("Juego: " + CompraTerminada.juego + " " + "precio: " + "$" + CompraTerminada.precio)
+    })
+  break;
+  }
+}
+
+const total = compras.reduce((ecc, el) => ecc + el.precio *1.71, 0 )
+console.log("El total de los juegos más la suma de impuestos es de: " + "$" + total)
 
 
+/* const JuegosConImpuestos=Juegos.map(juego => {
+  return {
+      nombre: Juego.nombre,
+      tipo: Juego.tipo,
+      precio: Juego.precio * 1.71
+  }
+/* ); */ 
 
+/* alert(JuegosConImpuestos) */
 
 //cuotas y funsion con iva
 /*function calcularIva( precio){
