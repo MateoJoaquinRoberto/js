fbe7da5a7cd08d5dbd4f7b6992d19d583681bffc
Mateo <mateojoaquinroberto@hotmail.com>
2022-08-17 23:34:48 -0300

    trabajo eventos

--- codigo.js
+++ codigo.js
@@ -1,6 +1,7 @@
 //Juegos disponobles 
 const Juegos=[
   {
+    id: 1,
     nombre:" Red dead redemption 2 ",
     tipo:"AAA",
     modo:"Singleplayer y Multiplayer",
@@ -10,6 +11,7 @@ const Juegos=[
   },
   
   {
+    id: 2,
     nombre:" The last Of Us ",
     tipo:"Aventura",
     modo:"Singleplayer",
@@ -19,6 +21,7 @@ const Juegos=[
 },
 
 {
+  id: 3,
   nombre:" Crushaders Kings III ",
   tipo:"Estrategia",
   modo:"Singleplayer y Multiplayer",
@@ -28,6 +31,7 @@ const Juegos=[
 },
 
 {
+  id: 4,
   nombre:" Sims 4",
   tipo:"Simulador",
   modo:"Singleplayer",
@@ -37,6 +41,7 @@ const Juegos=[
 },
 
 {
+  id: 5,
   nombre:" Gta V ",
   tipo:"Aventura",
   modo:"Singleplayer y Multiplayer",
@@ -46,6 +51,7 @@ const Juegos=[
 },                                                      
 
 {
+  id: 6,
   nombre:" NBA 2k23 ",
   tipo:"Deportes",
   modo:"Singleplayer y Multiplayer",
@@ -55,6 +61,7 @@ const Juegos=[
 },
 
 {
+  id: 7,
   nombre:" Hunt ",
   tipo:"Shotter",
   modo:"Singleplayer y Multiplayer",
@@ -64,6 +71,7 @@ const Juegos=[
 },
 
 {
+  id: 8,
   nombre:" Stray ",
   tipo:"Puzzles",
   modo:"Singleplayer",
@@ -78,94 +86,15 @@ const Juegos=[
 
 let compras=[]
 
-let VideosJuegos = prompt("Queres ver el catalogo de juegos?(Si o No) ")
 
-while(VideosJuegos!="Si" && VideosJuegos!="No"){
-  alert("Ingresa Si o No")
-  VideosJuegos = prompt("Queres ver el catalogo de juegos?(Si o No) ")
-}
-
-if(VideosJuegos=="Si"){
-  alert("Lista de Videojuegos disponibles a continuacion")
-  let TodosLosJuegos = Juegos.map((Juego)=>Juego.nombre + " " + "$" +Juego.precio  );
-alert(TodosLosJuegos.join("-"))
-} else if (VideosJuegos =="No"){ 
-alert("Gracias por visitarnos")
-}
-
-while(VideosJuegos!= "No"){
-  let juego = prompt("Agrega un Videojuego de la lista que quieras")
-  let precio = 0
-
-  if(juego == "Red dead redemption 2" || juego == "The last Of Us" || juego == "Crushaders Kings III"  || 
-  juego == "Sims 4" || juego == "Gta V" || juego == "NBA 2k23" || juego == "Hunt" || juego == "Stray") {  
-    switch(juego) { 
-    case "Red dead redemption 2":
-      precio = 2700
-      break;
-    case "The last Of Us":
-      precio = 1500;
-      break;
-    case "Crushaders Kings III":
-      precio = 3000;
-      break;
-    case "Sims 4":
-      precio = 2000;
-      break;
-    case "Gta V":
-      precio = 3000;
-      break;
-    case "NBA 2k23":
-      precio = 4000;
-      break;
-    case "Hunt":
-      precio = 1000;
-      break;
-    case "Stray":
-      precio = 500;
-      break;    
-    default:
-      break;          
-      
-    } 
-  compras.push({juego, precio}); 
-  console.log(compras)
-  } else{
-    alert("Juego no disponible")
-  }
-  VideosJuegos = prompt("Queres agregar otro juego al carrito?(Si o No)")
-
-  while(VideosJuegos == "No"){
-    alert("Gracias por tu compra,tu juego esta en tu carrito")
-    compras.forEach((CompraTerminada)=>{
-      console.log("Juego: " + CompraTerminada.juego + " " + "precio: " + "$" + CompraTerminada.precio)
-    })
-  break;
-  }
-}
-
-//Suma final con impuestos 
-const total = compras.reduce((ecc, el) => ecc + el.precio *1.71, 0 )
-console.log("El total de los juegos más la suma de impuestos es de: " + "$" + total)
 
 //doom
 console.dir(document.body)
 
-//getElementById (agrgado de textos y cambios del sitio)
-let fondo = document.getElementById("fondo");
-console.log(fondo.innerHTML);
-fondo.style.backgroundColor="grey";
 
- let titulo = document.getElementById("titulo")
-console.log( titulo.innerText )
-titulo.innerText="Bienvenido a Steamcito Julio"
-console.log(titulo.innerText); 
-
-let titulos= document.getElementById("titulo")
-titulo.innerHTML = "<h1>Steamcito</h1><h2>Bienvenido!!</h2><h3>Espero te guste el sitio Julio</h3>"
-
-
+renderizarProducto()
 //cartas
+function renderizarProducto(){ 
 let cartas=document.getElementById("cartas");
 for(const juego of Juegos){
   let carta=document.createElement("div");
@@ -178,55 +107,60 @@ for(const juego of Juegos){
   </div>
   <ul class="list-group list-group-flush">
   <li class="list-group-item">${juego.tipo}</li>
-            <li class="list-group-item">${juego.modo}</li>
-            <li class="list-group-item">${"desarrolador: "+ " "+juego.desarrollador}</li>
-            </ul>
-            <a id="miBoton" button class="btn btn-primary">Comprar</a>
-            `;
-            cartas.append(carta);
-          }
-          
-          //tabla
-          let tabla=document.createElement("table");
-          tabla.className="table table-striped";
-          let tablaBody=document.createElement("tbody");
-          for(const juego of Juegos){
-              tablaBody.innerHTML+=`
-              <tr>
-                  <td>${juego.nombre}</td>
-                  <td>${juego.tipo}</td>
-                  <td>${juego.precio}</td>
-              </tr>
-            `;
-           }
-          
-          tabla.append(tablaBody);
-          let articuloInferior = document.getElementById("inferior");
-          articuloInferior.append(tabla);
+  <li class="list-group-item">${juego.modo}</li>
+  <li class="list-group-item">${"desarrolador: "+ " "+juego.desarrollador}</li>
+  </ul>
+  <br>
+  <button class="btn btn-primary" id='btn${juego.id}'>Comprar</button>
+  `;
+  cartas.append(carta);
+}
 
-          //eventos(sin terminar )
- let miBoton=document.getElementById("miBoton");         
- miBoton.onclick=()=>{
-  alert("Producto agregado al carrito")
-  compras.push(Juegos.nombre)
+
+//eventos
+Juegos.forEach(juego=>{
+  document.getElementById(`btn${juego.id}`).addEventListener("click",function(){
+    agregarAlCarrito(juego);
+    })
+
+  })
+
+}
+
+
+//agregar al carrito
+function agregarAlCarrito(juego){
+  compras.push(juego);
   console.log(compras)
- }
-          
-        
- 
+
+  document.getElementById("inferior").innerHTML+=`
+    <tr>
+      <td>${juego.nombre}</td>
+      <td>${juego.desarrollador}</td>
+      <td>${juego.precio}</td>
+    </tr>`
+    alert("Producto agregado al carrito de compras")
+    
+    
+    //Suma final con impuestos 
+    const total = compras.reduce((ecc, el) => ecc + el.precio *1.71, 0 )
+    alert("El total de los juegos más la suma de impuestos es de: " + "$" + total)
+  }
+
+//cuotas (implimentar al final )
 /* alert(JuegosConImpuestos) */
 
 //cuotas y funsion con iva
 /*function calcularIva( precio){
   let precioConIva = precio * 1.75;
   return precioConIva
-
+  
   
   
   let conIva = calcularIva(parseInt(prompt("valor del juego sin impuestos")));
   alert("el precio con impuestos es $ "+conIva);
   
-
+  
   let cuota=parseInt(prompt("cuantas cuotas"))
   if(cuota==2){
     alert("cada cuota sera de $"+(precio+(precio*5/100))/2);
